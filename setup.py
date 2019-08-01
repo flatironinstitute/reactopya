de6c4568db4b99bb29d530ddb6e999ac7cc51f7a
Jeremy Magland <jeremy.magland@gmail.com>
2019-08-01 18:27:14 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+pkg_name = "reactopy"
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.1.0",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="",
+    packages=setuptools.find_packages(),
+    scripts=[],
+    install_requires=[],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    )
+)
