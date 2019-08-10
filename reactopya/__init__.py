61d15ff06815f5828452a97fe8079015c85e216d
Jeremy Magland <jeremy.magland@gmail.com>
2019-08-10 09:35:51 -0400

    incr version and fix templates issue

--- reactopya/__init__.py
+++ reactopya/__init__.py
@@ -1,6 +1,7 @@
 from .component import Component
 from .shellscript import ShellScript
 
-def reactopya_templates_directory();
+def reactopya_templates_directory():
+    import os
     dirname = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(dirname, '..', 'templates')
+    return os.path.join(dirname, 'templates')
